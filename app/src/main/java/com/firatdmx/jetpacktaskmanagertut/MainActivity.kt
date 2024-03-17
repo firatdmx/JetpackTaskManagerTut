ca3d074cf2adaa606decd4e3919579af06fe66b8
firatdmx <firatduman91@gmail.com>
2024-03-17 11:32:11 +0300

    Add composables

--- app/src/main/java/com/firatdmx/jetpacktaskmanagertut/MainActivity.kt
+++ app/src/main/java/com/firatdmx/jetpacktaskmanagertut/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.firatdmx.jetpacktaskmanagertut
 
+import android.graphics.fonts.FontStyle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.firatdmx.jetpacktaskmanagertut.ui.theme.JetpackTaskManagerTutTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //TODO
+                    TaskManager(modifier = Modifier)
                 }
             }
         }
@@ -30,11 +42,38 @@ class MainActivity : ComponentActivity() {
 }
 
 
+@Composable
+fun TaskManager(modifier: Modifier = Modifier){
+    val image = painterResource(id = R.drawable.ic_task_completed)
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .fillMaxSize()
+            .padding(5.dp)
+    )
+    {
 
-@Preview(showBackground = true)
+        Image(painter = image, contentDescription = "done")
+        Text(
+            text = stringResource(id = R.string.completed),
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .padding(top = 24.dp, bottom = 8.dp)
+        )
+        Text(
+            text = stringResource(id = R.string.nicew),
+            fontSize = 16.sp
+        )
+    }
+}
+
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     JetpackTaskManagerTutTheme {
-        //TODO
+        TaskManager(modifier = Modifier)
     }
 }
